52eba69bfd3965e5c84d9ebd1011868536e03194
smds <root@deceptsoft.com>
2023-12-29 00:59:46 +0100

    Blinky cursor fix

--- src/IRQ.c
+++ src/IRQ.c
@@ -5,6 +5,7 @@
 
 static u8 CursorBlink = 0; // Cursor blink counter
 extern SM_Device DEV_UART;
+extern u8 FontSize;
 u16 Cursor_CL = 0x0E0;
 
 void VB_IRQ()
@@ -12,12 +13,16 @@ void VB_IRQ()
     // Cursor blink
     if (CursorBlink == 20)
     {
-        PAL_setColor(31, Cursor_CL);
+        if (FontSize) PAL_setColor(31, Cursor_CL);
+        else PAL_setColor(2, Cursor_CL);
+
         CursorBlink++;
     }
     else if (CursorBlink == 40)
     {
-        PAL_setColor(31, 0x000);
+        if (FontSize) PAL_setColor(31, 0x000);
+        else PAL_setColor(2, 0x000);
+        
         CursorBlink = 0;
     }
     else CursorBlink++;
